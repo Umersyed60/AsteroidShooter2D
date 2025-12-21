72b210c109fa9abb13ab75bda9f5b0adf7c3c7c7
umer-aftab <umersyed2014@gmail.com>
2025-12-21 20:49:38 +0500

    Implementation of player controls
    Implementation of player controls and addition of ui assets.

--- Assets/Code/Controllers/PlayerController.cs
+++ Assets/Code/Controllers/PlayerController.cs
@@ -8,19 +8,24 @@ namespace MyPractice.Examples.Controllers
     {
         #region Variables
         private readonly IInputHandler _inputHandler;
+        private readonly IGameSettings _gameSettings;
+        private readonly IPlayerView _playerView;
         #endregion
 
         #region Custom Methods
-        public PlayerController(IInputHandler inputHandler)
+        public PlayerController(IInputHandler inputHandler, IGameSettings gameSettings, IPlayerView playerView)
         {
             _inputHandler = inputHandler;
+            _gameSettings = gameSettings;
+            _playerView = playerView;
         }
 
         public void Tick()
         {
             if (!Mathf.Approximately(_inputHandler.AimDirection, 0f))
             {
-                Debug.Log($"Aim Direction: {_inputHandler.AimDirection}");
+                //Debug.Log($"Aim Direction: {_inputHandler.AimDirection}");
+                _playerView.CannonRotation += _gameSettings.AimRotationDegreesPerSecond * Time.deltaTime * _inputHandler.AimDirection;
             }
 
             if (_inputHandler.IsFireButtonPressed)
